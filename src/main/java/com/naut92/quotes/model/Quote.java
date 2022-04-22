c480039f8d47a2d185ff90d8570d53b0489dee22
naut <naut92@gmail.com>
2022-04-22 19:38:03 +0200

    models

--- src/main/java/com/naut92/quotes/model/Quote.java
+++ src/main/java/com/naut92/quotes/model/Quote.java
@@ -6,10 +6,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import java.time.Instant;
 
 @Getter
 @Setter
@@ -17,7 +19,18 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "quote")
 public class Quote extends AbstractPersistable<Long> {
+
+    @Column(name = "text")
     private String text;
+
+    @Column(name = "like")
+    private Long like;
+
+    @Column(name = "dislike")
+    private Long dislike;
+
+    @Column(name = "timestamp")
+    private Instant timestamp;
 
     @JsonBackReference
     @ManyToOne
