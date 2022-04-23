4a73846c54c3c46bc48f6572f1aec992fac00450
naut <naut92@gmail.com>
2022-04-23 02:43:14 +0200

    data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,4 +6,24 @@ INSERT INTO users (firstname, lastname, email) VALUES
 ('firstname4', 'lastname4', 'email4');
 
 --Quotes
-INSERT INTO quotes (text, topic, like_q, dislike, timestamp, user_id) VALUES;
+INSERT INTO quotes (text, topic, like_q, dislike, timestamp, user_id) VALUES
+('text1', 'SOCIETY', 120, 4, '', 1);
+('text2', 'SOCIETY', 12, 3, '', 1);
+('text3', 'SOCIETY', 10, 2, '', 1);
+('text4', 'SOCIETY', 12, 0, '', 2);
+('text5', 'SOCIETY', 12, 10, '', 2);
+('text6', 'SOCIETY', 24, 100, '', 3);
+('text7', 'SOCIETY', 124, 16, '', 4);
+
+
+('text8', 'CULTURE', 12, 4, '', 1);
+('text9', 'CULTURE', 26, 4, '', 1);
+('text10', 'CULTURE', 130, 4, '', 2);
+('text11', 'CULTURE', 1, 4, '', 2);
+('text12', 'CULTURE', 129, 34, '', 2);
+('text13', 'CULTURE', 17, 41, '', 2);
+('text14', 'CULTURE', 22, 4, '', 3);
+('text15', 'CULTURE', 1276, 18, '', 4);
+('text16', 'CULTURE', 128, 45, '', 4);
+
+
